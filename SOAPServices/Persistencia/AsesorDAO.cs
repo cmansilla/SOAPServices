4ec8b00e92518786172439b59631a39b1fa528d7
christian <christian.mansilla@gmail.com>
2013-09-17 02:46:11 -0500

    ok primera subida
    Signed-off-by: christian <christian.mansilla@gmail.com>

---
+++ SOAPServices/Persistencia/AsesorDAO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SOAPServices.Dominioo;
+
+namespace SOAPServices.Persistencia
+{
+    public class AsesorDAO : BaseDAO<Asesor, int>
+    {
+    }
+}
